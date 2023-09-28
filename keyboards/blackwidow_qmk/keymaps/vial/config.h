18cf4593a9596a5abb2734c4a26034253620fbfd
Dharon Yusuf Fuadi <yusuffuadidharon@gmail.com>
2023-09-28 09:02:26 +0700

    Changed Debounce to Eager_pr

---
+++ keyboards/blackwidow_qmk/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEB, 0xEC, 0x7B, 0xA0, 0x06, 0xE0, 0xBB, 0xBC}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
--- keyboards/blackwidow_qmk/rules.mk
+++ keyboards/blackwidow_qmk/rules.mk
@@ -1,3 +1,4 @@
 EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = embedded_flash
 KEY_LOCK_ENABLE = yes
+DEBOUNCE_TYPE = sym_eager_pr
